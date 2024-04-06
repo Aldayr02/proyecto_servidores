3cc7ddfe6fa97dd0d26b4fefe5968e2520954921
Uziel-Iteso <123020579+UzielSolis@users.noreply.github.com>
2024-04-06 17:11:51 -0600

    Delete dist directory

--- dist/routes/users.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const users_controllers_1 = require("../controllers/users.controllers");
-const router = require('express').Router();
-const userController = new users_controllers_1.UsersController();
-router.post('/signUp', userController.register);
-//router.post('/login', userController.login);
-exports.default = router;
--- dist/utils/roles-status.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-module.exports = {
-    ADMIN: 'admin',
-    USER: 'user',
-    AUTHOR: 'author'
-};
