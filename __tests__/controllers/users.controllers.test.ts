933744d77cdf2f9a70f09cf5a9f56f186ff14306
Uziel-Iteso <123020579+UzielSolis@users.noreply.github.com>
2024-05-08 06:02:32 -0600

    Google button (#11)
    * fix: :art: Modify upload middleware
    
    erasing console logs lines
    
    * feat: :rocket: testing one hundred percent
    
    testing user controllers

--- __tests__/controllers/users.controllers.test.ts
+++ __tests__/controllers/users.controllers.test.ts
@@ -1,192 +1,167 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
 
-import { UsersController } from '../../src/controllers/users.controllers';
-import userModel from '../../src/models/users.models';
-import { response_status } from '../../src/utils/response_status';
-import { create } from '../../src/utils/token';
+import { UsersController } from "../../src/controllers/users.controllers";
+import userModel from "../../src/models/users.models";
+import { response_status } from "../../src/utils/response_status";
 
 const userController = new UsersController();
 
 // Mock the external dependencies
-jest.mock('../../src/models/users.models', () => ({
-  create: jest.fn().mockResolvedValue(Promise.resolve('User created')),
-  findOne: jest.fn().mockResolvedValue(
-    Promise.resolve({
-      name: 'TestUser',
-      password: 'password123', // `pragma: allowlist secret`
-      email: 'test@example.com',
-      role: 'admin',
-    })
-  ),
-  updateOne: jest.fn().mockResolvedValue(Promise.resolve('')),
+jest.mock("../../src/models/users.models", () => ({
+	create: jest.fn().mockResolvedValue(Promise.resolve("User created")),
+	findOne: jest.fn().mockResolvedValue(
+		Promise.resolve({
+			name: "TestUser",
+			password: "password123",
+			email: "test@example.com",
+      role: "admin"
+		}),
+	),
+	updateOne: jest.fn().mockResolvedValue(Promise.resolve("")),
 }));
 
-jest.mock("../../src/utils/token.ts", () => ({
-  code: jest.fn().mockReturnValue("token"),
-}));
+describe("UsersController", () => {
+	describe("register", () => {
+		let req: Partial<Request>;
+		let res: Partial<Response>;
 
-// Mocking jwt.sign directamente si es necesario
-jest.mock('jsonwebtoken', () => ({
-  sign: jest.fn().mockReturnValue('mocked_token')
-}));
+		// Mock the Express req and res objects before each test
+		beforeEach(() => {
+			req = {
+				body: {
+					name: "TestUser",
+					password: "password123",
+					email: "test@example.com",
+          role: "admin"
+				},
+			};
 
-// Mocking del módulo que contiene la función createToken
-jest.mock('../../src/utils/token', () => ({
-  createToken: jest.fn().mockReturnValue('mocked_token') // Asegúrate de que este token coincida con tus expectativas en las pruebas
-}));
+			res = {
+				status: jest.fn().mockReturnThis(),
+				send: jest.fn().mockReturnThis(),
+			};
+		});
 
-describe('UsersController', () => {
-  describe('register', () => {
-    let req: Partial<Request>;
-    let res: Partial<Response>;
+		test("should create a new user and send a CREATED response", async () => {
+			// Arrange
+			// Cast the req object to the correct type
+			await userController.register(req as Request, res as Response);
 
-    // Mock the Express req and res objects before each test
-    beforeEach(() => {
-      req = {
-        body: {
-          name: 'TestUser',
-          password: 'password123',
-          email: 'test@example.com',
-          role: 'admin',
-        },
-      };
+			// Assert
+			expect(userModel.create).toHaveBeenCalledWith({
+                name: "TestUser",
+                password: "password123",
+                email: "test@example.com",
+                role: "admin"
+			});
+			expect(res.status).toHaveBeenCalledWith(response_status.CREATED);
+			expect(res.send).toHaveBeenCalledWith("User created");
+		});
 
-      res = {
-        status: jest.fn().mockReturnThis(),
-        send: jest.fn().mockReturnThis(),
-      };
-    });
+		test("should handle 'email already exists' error", async () => {
+			// Arrange
 
-    test('should create a new user and send a CREATED response', async () => {
-      // Arrange
-      // Cast the req object to the correct type
-      await userController.register(req as Request, res as Response);
+			const createMock = userModel.create as jest.Mock;
+			createMock.mockRejectedValueOnce({ code: 11000 });
 
-      // Assert
-      expect(userModel.create).toHaveBeenCalledWith({
-        name: 'TestUser',
-        password: 'password123',
-        email: 'test@example.com',
-        role: 'admin',
-      });
-      expect(res.status).toHaveBeenCalledWith(response_status.CREATED);
-      expect(res.send).toHaveBeenCalledWith('User created');
-    });
+			try {
+				// Act
+				await userController.register(req as Request, res as Response);
+			} catch (error) {
+				// Assert
+				expect(res.status).toHaveBeenCalledWith(response_status.BAD_REQUEST);
+				expect(res.send).toHaveBeenCalledWith("Email already exists");
+			}
+		});
 
-    test("should handle 'email already exists' error", async () => {
-      // Arrange
+		test("should handle validation errors", async () => {
+			// Arrange
 
-      const createMock = userModel.create as jest.Mock;
-      createMock.mockRejectedValueOnce({ code: 11000 });
+			const createMock = userModel.create as jest.Mock;
+			const validationError = new Error("Validation error");
+			validationError.name = "ValidationError";
+			createMock.mockRejectedValueOnce(validationError);
 
-      try {
-        // Act
-        await userController.register(req as Request, res as Response);
-      } catch (error) {
-        // Assert
-        expect(res.status).toHaveBeenCalledWith(response_status.BAD_REQUEST);
-        expect(res.send).toHaveBeenCalledWith('Email already exists');
-      }
-    });
+			try {
+				// Act
+				await userController.register(req as Request, res as Response);
+			} catch (error) {
+				// Assert
+				expect(res.status).toHaveBeenCalledWith(response_status.BAD_REQUEST);
+				expect(res.send).toHaveBeenCalledWith("Invalid data");
+			}
+		});
 
-    test('should handle validation errors', async () => {
-      // Arrange
+		test("should handle generic errors", async () => {
+			// Arrange
 
-      const createMock = userModel.create as jest.Mock;
-      const validationError = new Error('Validation error');
-      validationError.name = 'ValidationError';
-      createMock.mockRejectedValueOnce(validationError);
+			const createMock = userModel.create as jest.Mock;
+			createMock.mockRejectedValueOnce(new Error("Generic error"));
 
-      try {
-        // Act
-        await userController.register(req as Request, res as Response);
-      } catch (error) {
-        // Assert
-        expect(res.status).toHaveBeenCalledWith(response_status.BAD_REQUEST);
-        expect(res.send).toHaveBeenCalledWith('Invalid data');
-      }
-    });
+			try {
+				// Act
+				await userController.register(req as Request, res as Response);
+			} catch (error) {
+				// Assert
+				expect(res.status).toHaveBeenCalledWith(
+					response_status.BAD_REQUEST,
+				);
+				expect(res.send).toHaveBeenCalledWith("Something went wrong");
+			}
+		});
+	});
 
-    test('should handle generic errors', async () => {
-      // Arrange
+	describe("login", () => {
+		let req: Partial<Request>;
+		let res: Partial<Response>;
 
-      const createMock = userModel.create as jest.Mock;
-      createMock.mockRejectedValueOnce(new Error('Generic error'));
+		// Mock the Express req and res objects before each test
+		beforeEach(() => {
+			req = {
+				body: {
+					name: "TestUser",
+					password: "password123",
+					email: "test@example.com",
+				},
+			};
 
-      try {
-        // Act
-        await userController.register(req as Request, res as Response);
-      } catch (error) {
-        // Assert
-        expect(res.status).toHaveBeenCalledWith(response_status.BAD_REQUEST);
-        expect(res.send).toHaveBeenCalledWith('Something went wrong');
-      }
-    });
-  });
+			res = {
+				status: jest.fn().mockReturnThis(),
+				send: jest.fn().mockReturnThis(),
+			};
+		});
+		test("should log in an existing user and send back a token", async () => {
+			// Arrange
+			(userModel.findOne as jest.Mock).mockResolvedValue({
+				name: "TestUser",
+				email: "test@example.com",
+			});
 
-  describe('login', () => {
-    let req: Partial<Request>;
-    let res: Partial<Response>;
+			// Act
+			await userController.login(req as Request, res as Response);
+			// Assert
+			expect(userModel.findOne).toHaveBeenCalledWith({
+				email: "test@example.com",
+				password: "password123",
+			});
+			expect(res.status).toHaveBeenCalledWith(response_status.SUCCESS);
+			expect(res.send).toHaveBeenCalledWith({ token: "123" });
+		});
 
-    // Mock the Express req and res objects before each test
-    beforeEach(() => {
-      req = {
-        body: {
-          name: 'TestUser',
-          password: 'password123',
-          email: 'test@example.com',
-        },
-      };
+		test("should handle generic errors", async () => {
+			// Arrange
+			(userModel.findOne as jest.Mock).mockRejectedValue(new Error("Generic error"));
 
-      res = {
-        status: jest.fn().mockReturnThis(),
-        send: jest.fn().mockReturnThis(),
-      };
-    });
-    test('should log in an existing user and send back a token', async () => {
-      // Arrange
-      (userModel.findOne as jest.Mock).mockResolvedValue({
-        name: 'TestUser',
-        email: 'test@example.com',
-      });
-
-      // Act
-      await userController.login(req as Request, res as Response);
-
-      // Assert
-      expect(userModel.findOne).toHaveBeenCalledWith({
-        email: 'test@example.com',
-        password: 'password123',
-      });
-      expect(create).toHaveBeenCalled();
-      expect(res.status).toHaveBeenCalledWith(response_status.SUCCESS);
-      expect(res.send).toHaveBeenCalledWith({ token: 'token' });
-    });
-
-    test('should handle invalid credentials', async () => {
-      // Arrange
-      (userModel.findOne as jest.Mock).mockResolvedValue(null);
-
-      // Act
-      await userController.login(req as Request, res as Response);
-
-      // Assert
-      expect(res.status).toHaveBeenCalledWith(response_status.UNAUTHORIZED);
-      expect(res.send).toHaveBeenCalledWith('failed to login');
-    });
-
-    test('should handle generic errors', async () => {
-      // Arrange
-      (userModel.findOne as jest.Mock).mockRejectedValue(new Error('Generic error'));
-
-      // Act
-      try {
-        await userController.login(req as Request, res as Response);
-      } catch (error) {
-        // Assert
-        expect(res.status).toHaveBeenCalledWith(response_status.BAD_REQUEST);
-        expect(res.send).toHaveBeenCalledWith('Something went wrong');
-      }
-    });
-  });
+			// Act
+			try {
+				await userController.login(req as Request, res as Response);
+			} catch (error) {
+				// Assert
+				expect(res.status).toHaveBeenCalledWith(
+					response_status.BAD_REQUEST,
+				);
+				expect(res.send).toHaveBeenCalledWith("Something went wrong");
+			}
+		});
+	});
 });
--- src/utils/hash_password.ts
+++ src/utils/hash_password.ts
@@ -4,3 +4,10 @@ export default function hashPassword(pwd: string = ''): string {
     const hashedPwd: Buffer = scryptSync(pwd, 'admin', 24);
     return hashedPwd.toString('hex');
 }
+
+export function verifyPassword(storedHash: string, passwordToCheck: string): boolean {
+    const [salt, hash] = storedHash.split(':'); // Separamos la sal y el hash
+    const hashedBuffer: Buffer = scryptSync(passwordToCheck, salt, 24);
+    const hashedPassword = hashedBuffer.toString('hex');
+    return hash === hashedPassword; // Comparamos los hashes
+}
